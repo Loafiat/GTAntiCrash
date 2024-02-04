663c4a379b76a9049b42a9fd9bd1106eba338a31
Lofiat <75358872+dyeddaboi@users.noreply.github.com>
2024-02-03 22:10:47 -0600

    first commit
    added all code

---
+++ Plugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using BepInEx;
+using Photon.Pun.UtilityScripts;
+using UnityEngine;
+using Utilla;
+using static SinglePool;
+
+namespace AntiCrash
+{
+	[ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla")]
+	[BepInPlugin("Lofiat.AntiCrash", "AntiCrash", "1.0.0")]
+	public class Plugin : BaseUnityPlugin
+	{
+        bool inRoom;
+        Transform ObjectPools, SodaBubbles, playerTransform;
+        SinglePool BubblePool;
+        void Start() => Utilla.Events.GameInitialized += OnGameInitialized;
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            ObjectPools = GameObject.Find("Environment Objects/PersistentObjects_Prefab/GlobalObjectPools").transform;
+            playerTransform = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player").transform;
+            SodaBubbles = GameObject.Find("Environment Objects/PersistentObjects_Prefab/GlobalObjectPools/SodaBubble(Clone)").transform;
+            BubblePool = ObjectPools.GetComponent<ObjectPools>().GetPoolByObjectType(SodaBubbles.gameObject);
+        }
+
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+            BubblePool.initAmountToPool = 0;
+            BubblePool.Initialize(ObjectPools.gameObject);
+        }
+
+        void Update()
+        {
+            if (inRoom)
+            {
+                if (Time.deltaTime == 2000)
+                {
+                    ObjectPools.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        [ModdedGamemodeLeave]
+		public void OnLeave(string gamemode)
+        {
+            inRoom = true;
+            foreach (Transform Bubbles in SodaBubbles)
+            {
+                Destroy(Bubbles.gameObject);
+            }
+            BubblePool.initAmountToPool = 256;
+            BubblePool.Initialize(ObjectPools.gameObject);
+            SodaBubbles = GameObject.Find("Environment Objects/PersistentObjects_Prefab/GlobalObjectPools/SodaBubble(Clone)").transform;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Makes it impossible to be crashed in Modded lobbies!
 ## Thank you Octo/Evelyn for helping clean the code!
 ### NOTE: MAY CAUSE LAG WHEN JOINING/LEAVING MODDEDS, THIS LAG IS TEMPORARY!
+
+
+**If you are still getting lagged simply wait, the issue should resolve itself automatically. If not, report it to the discord, along with the crash logs if you can**
